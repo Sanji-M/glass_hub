4c3399a72a701d0263645404d01ffb1399e7d21a
Sanji-M <eternalrogue3@gmail.com>
2026-06-13 16:33:48 +0300

    migrated to PostgreSQL

--- core/settings.py
+++ core/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'glasshub',
+        'USER': 'glasshub_user',
+        'PASSWORD': 'Sanji@7166gb',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
--- hub/views.py
+++ hub/views.py
@@ -1,4 +1,3 @@
-```python
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib.auth import login
