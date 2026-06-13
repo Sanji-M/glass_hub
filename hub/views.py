ce26101f528b0bc5402d3daac3e98b9cc61a4641
Sanji-M <eternalrogue3@gmail.com>
2026-06-13 15:14:47 +0300

    just moved templates to project level dir

--- hub/views.py
+++ hub/views.py
@@ -1,6 +1,10 @@
+```python
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib.auth import login
 from django.shortcuts import render,redirect
 from .forms import SignupForm
+
 # My views
 @login_required
 def dashboard(request):
@@ -12,10 +16,28 @@ def dashboard(request):
 def signup_view(request):
     if request.method == 'POST':
         form = SignupForm(request.POST)  #form filled with data from the request
+        
         if form.is_valid():
-            return render(request,'hub/login.html')
+            full_name = form.cleaned_data['full_name']
+            password1 = form.cleaned_data['password1']
+            
+         
+            #blueprint for creating a user
+            parts  = full_name.strip().split(' ',1)
+            first_name = parts[0]
+            last_name = parts[1] if len(parts) > 1 else ''
+            username = full_name.lower().replace(' ', '_')  # Replace spaces with underscores for username
 
+            user = User.objects.create_user(
+                username=username,
+                first_name=first_name,
+                last_name=last_name,
+                password=password1
+            )
 
+            login(request, user)  # Log the user in after successful signup
+            return redirect('dashboard')  # Redirect to the dashboard after signup
     else:
+        form = SignupForm()  
 
-        return render(request,'hub/signup.html')
+    return render(request,'hub/signup.html',{'form':form})
